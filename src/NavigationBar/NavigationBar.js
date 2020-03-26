99f536934e7f0529d73bb18a37d7b7d161c60338
PrOrOk26 <vladgold2699@gmail.com>
2020-03-26 13:30:05 +0200

    BS: Add changes to Navigation bar

--- src/NavigationBar/NavigationBar.js
+++ src/NavigationBar/NavigationBar.js
@@ -1,4 +1,8 @@
 import createElement from "../shared/components.js";
+import TwitterIcon from "./assets/twitter.svg";
+import FacebookIcon from "./assets/facebook.svg";
+import InstagramIcon from "./assets/instagram.svg";
+import YoutubeIcon from "./assets/youtube.svg";
 
 const NavigationBar = props => {
   const { onNavbarToggle } = props;
@@ -36,6 +40,29 @@ const NavigationBar = props => {
     }
   ];
 
+  const socialMedia = [
+    {
+      name: "Facebook",
+      icon: FacebookIcon,
+      url: "https://www.facebook.com/BlackSabbath"
+    },
+    {
+      name: "Twitter",
+      icon: TwitterIcon,
+      url: "https://twitter.com/blacksabbath"
+    },
+    {
+      name: "Instagram",
+      icon: InstagramIcon,
+      url: "https://www.instagram.com/blacksabbath/"
+    },
+    {
+      name: "YouTube",
+      icon: YoutubeIcon,
+      url: "https://www.youtube.com/channel/UCrx-X329UKv0Y06VhfpFVvw"
+    }
+  ];
+
   return createElement(
     "nav",
     { class: "navbar" },
@@ -55,14 +82,16 @@ const NavigationBar = props => {
             {
               class: "main-category__link",
               href: "#",
-              onclick: elem.subElements && (e => {
-                const subcategoriesContainer = e.path.find(elem => elem.className === 'main-category').getElementsByClassName(
-                  "subcategories"
-                )[0];
-                if (subcategoriesContainer) {
-                  subcategoriesContainer.classList.toggle("active");
-                }
-              })
+              onclick:
+                elem.subElements &&
+                (e => {
+                  const subcategoriesContainer = e.path
+                    .find(elem => elem.className === "main-category")
+                    .getElementsByClassName("subcategories")[0];
+                  if (subcategoriesContainer) {
+                    subcategoriesContainer.classList.toggle("active");
+                  }
+                })
             },
             createElement("span", {
               innerText: elem.name
@@ -95,6 +124,47 @@ const NavigationBar = props => {
             )
         );
       })
+    ),
+    createElement(
+      "div",
+      {
+        class: "social-media"
+      },
+      createElement("span",
+      {
+        class: 'social-media__header-text',
+        innerText: 'Follow us on social media'
+      }),
+      createElement(
+        "ul",
+        {},
+        ...socialMedia.map(elem => {
+          return createElement(
+            "li",
+            {},
+            createElement(
+              "a",
+              {
+                href: elem.url,
+                target: "_blank"
+              },
+              createElement(
+                "i",
+                {},
+                createElement("span", {
+                  class: "visually-hidden",
+                  innerText: elem.name
+                }),
+                createElement("img", {
+                  class: "sm-icon",
+                  src: elem.icon,
+                  alt: elem.name
+                })
+              )
+            )
+          );
+        })
+      )
     )
   );
 };
